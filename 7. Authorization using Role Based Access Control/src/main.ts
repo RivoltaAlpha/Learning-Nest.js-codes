1b219e54237a4b511fbbb653bc8604135728a09d
KevinComba <kelchospense88@gmail.com>
2025-06-04 01:23:52 +0300

    feat: Implement Lecturer and Student modules with Role-Based Access Control
    - Added Lecturer entity with relationships to Profile and Course.
    - Created LecturerController and LecturerService for CRUD operations and course assignments.
    - Developed Student entity with relationships to Profile and Course.
    - Implemented StudentsController and StudentsService for student management and course enrollments.
    - Introduced Profile entity with role management and relationships to Student and Lecturer.
    - Established DTOs for creating and updating Lecturer, Student, and Profile entities.
    - Integrated logger middleware for request logging.
    - Set up global exception handling and validation pipes in main application.
    - Configured TypeORM for database interactions and relationships.
    - Added necessary module imports and exports for proper dependency injection.

---
+++ 7. Authorization using Role Based Access Control/src/main.ts
@@ -0,0 +1,20 @@
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AllExceptionsFilter } from './http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  // Register the global exception filter
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.getOrThrow<number>('PORT');
+
+  await app.listen(PORT);
+}
+bootstrap();
